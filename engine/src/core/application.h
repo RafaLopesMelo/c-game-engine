f7fc01bb42e9c2a5b645130fb62d50e86dc6ac2c
Rafael Melo <rafaelmelo.programmer@gmail.com>
2025-06-19 19:30:04 -0300

    improve entrypoint

--- engine/src/core/application.h
+++ engine/src/core/application.h
@@ -2,6 +2,8 @@
 
 #include "defines.h"
 
+struct game;
+
 typedef struct application_config {
     // Window starting position x axis, if applicable
     i16 start_pos_x;
@@ -19,6 +21,6 @@ typedef struct application_config {
     char *name;
 } application_config;
 
-KAPI b8 application_create(application_config *config);
+KAPI b8 application_create(struct game *game_inst);
 
 KAPI b8 application_run();
